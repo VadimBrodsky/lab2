4c270328488f6ea769b97525203affb9395e6db5
Vadim Brodsky <vadim.brodsky@vidyard.com>
2017-07-07 13:35:25 -0400

    Use redux provider and store

--- js/react-native/fm-react-native-redux/react-native-todo/App.js
+++ js/react-native/fm-react-native-redux/react-native-todo/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Todo from './src/Todo';
 import { HTTPExample } from './src/httpExample';
+import { Provider } from 'react-redux';
+import { store } from './src/store';
 
-const App = () => <Todo />
+const App = () => (
+  <Provider store={store}>
+    <Todo />
+  </Provider>
+);
 
 export default App;
 

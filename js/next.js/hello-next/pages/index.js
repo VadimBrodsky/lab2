20e66bc39d2194fd0ce56eb991358dc3012b840f
Vadim Brodsky <vadim.brodsky@vidyard.com>
2017-07-17 14:49:54 -0400

    Add style to the postLink component

--- js/next.js/hello-next/pages/index.js
+++ js/next.js/hello-next/pages/index.js
@@ -9,32 +9,13 @@ function getPosts() {
   ];
 }
 
-const PostLink = (props) => (
-  <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-    <a>{props.title}</a>
-  </Link>
-);
-
-export default () => (
-  <Layout>
-    <h1>Next Blog</h1>
-    <ul>
-      {getPosts().map((post) => (
-        <li key={post.id}>
-          <PostLink id={post.id} title={post.title} />
-        </li>
-      ))}
-    </ul>
+const PostLink = ({ post }) => (
+  <li>
+    <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
+      <a>{post.title}</a>
+    </Link>
     <style jsx>
       {`
-        h1, a {
-          font-family: 'Arial';
-        }
-
-        ul {
-          padding: 0;
-        }
-
         li {
           list-style: none;
           margin: 5px 0;
@@ -43,10 +24,33 @@ export default () => (
         a {
           text-decoration: none;
           color: blue;
+          font-family: 'Arial';
         }
 
         a:hover {
           opacity: 0.6;
+        }
+      `}
+    </style>
+  </li>
+);
+
+export default () => (
+  <Layout>
+    <h1>Next Blog</h1>
+    <ul>
+      {getPosts().map((post) => (
+        <PostLink key={post.id} post={post} />
+      ))}
+    </ul>
+    <style jsx>
+      {`
+        h1 {
+          font-family: 'Arial';
+        }
+
+        ul {
+          padding: 0;
         }
       `}
     </style>

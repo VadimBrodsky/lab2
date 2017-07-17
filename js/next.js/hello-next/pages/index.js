7f589eb14f05aa605e10fe620fd4e227e6d13b55
Vadim Brodsky <vadim.brodsky@vidyard.com>
2017-07-17 14:44:49 -0400

    Add Style jsx

--- js/next.js/hello-next/pages/index.js
+++ js/next.js/hello-next/pages/index.js
@@ -1,21 +1,54 @@
 import Layout from '../components/MyLayout';
 import Link from 'next/link';
 
+function getPosts() {
+  return [
+    { id: 'hello-nextjs', title: 'Hello Next.js' },
+    { id: 'winter-is-coming', title: 'Winter is coming' },
+    { id: 'another-one', title: 'Another One' },
+  ];
+}
+
 const PostLink = (props) => (
-  <li>
-    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
-    </Link>
-  </li>
+  <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+    <a>{props.title}</a>
+  </Link>
 );
 
 export default () => (
   <Layout>
     <h1>Next Blog</h1>
     <ul>
-      <PostLink id="hello-nextjs" title="Hello Next.js" />
-      <PostLink id="winter-is-coming" title="Winter is coming" />
-      <PostLink id="another-one" title="Another One" />
+      {getPosts().map((post) => (
+        <li key={post.id}>
+          <PostLink id={post.id} title={post.title} />
+        </li>
+      ))}
     </ul>
+    <style jsx>
+      {`
+        h1, a {
+          font-family: 'Arial';
+        }
+
+        ul {
+          padding: 0;
+        }
+
+        li {
+          list-style: none;
+          margin: 5px 0;
+        }
+
+        a {
+          text-decoration: none;
+          color: blue;
+        }
+
+        a:hover {
+          opacity: 0.6;
+        }
+      `}
+    </style>
   </Layout>
 );

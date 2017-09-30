3283627fc10a47362d63d003ef458c6597c9a016
Vadim Brodsky <vadim.brodsky@gmail.com>
2017-09-30 13:09:09 -0400

    Extract inputs to Field component

--- js/react/fullstack-react/forms/src/basic-input.js
+++ js/react/fullstack-react/forms/src/basic-input.js
@@ -1,8 +1,49 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function isEmail(email) {
   return !!email.match(/^[^@\s]+@[^@\s]+$/);
 
+}
+
+class Field extends React.Component {
+  static propTypes = {
+    placeholder: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    validate: PropTypes.func,
+    onChange: PropTypes.func.isRequired,
+  }
+
+  state = {
+    value: this.props.value,
+    error: false,
+  }
+
+  onChange = evt => {
+    const name = this.props.name;
+    const value = evt.target.value;
+    const error = this.props.validate ? this.props.validate(value) : false;
+    this.setState({ value, error });
+    this.props.onChange({ name, value, error });
+  }
+
+  componentWillReceiveProps(update) {
+    this.setState({ value: update.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          placehodler={this.props.placeholder}
+          value={this.props.value}
+          onChange={this.onChange}
+        />
+        <span class="pure-form-message" style={{ color: 'red' }}>{ this.state.error }</span>
+      </div>
+    );
+  }
 }
 
 export default class BasicInput extends React.Component {

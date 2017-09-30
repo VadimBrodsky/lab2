9e63bf1445123c0c4fd67c78f7a599a9294aa2c4
Vadim Brodsky <vadim.brodsky@gmail.com>
2017-09-29 22:17:10 -0400

    Abstract on click handlers to one method

--- js/react/fullstack-react/forms/src/basic-button.js
+++ js/react/fullstack-react/forms/src/basic-button.js
@@ -2,23 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export default class BasicButton extends React.Component {
-  onGreatClick = (evt)  => {
-    console.log('The user clicked button-1: great', evt);
-  }
-
-  onAmazingClick = (evt) => {
-    console.log('The user clicked button-2: amazing', evt);
+  onButtonClick = (evt) => {
+    const btn = evt.target;
+    console.log(`The user clicked ${btn.name}: ${btn.value}`);
   }
 
   render() {
     return (
       <div>
         <h1>What do you think of React?</h1>
-        <button name="button-1" value="great" onClick={this.onGreatClick}>
+        <button name="button-1" value="great" onClick={this.onButtonClick}>
           Great
         </button>
 
-        <button name="button-2" value="amazing" onClick={this.onAmazingClick}>
+        <button name="button-2" value="amazing" onClick={this.onButtonClick}>
           Amazing
         </button>
       </div>

c3730cc73d91150aace4ec1a88cd8af94b88e5f6
Vadim Brodsky <vadim.brodsky@gmail.com>
2017-09-29 22:26:17 -0400

    Add BasicInput to page

--- js/react/fullstack-react/forms/src/index.js
+++ js/react/fullstack-react/forms/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import BasicButton from './basic-button';
+import BasicInput from './basic-input';
 
 const Page = () => {
   return (
     <div>
       <BasicButton />
+      <BasicInput />
     </div>
   )
 }

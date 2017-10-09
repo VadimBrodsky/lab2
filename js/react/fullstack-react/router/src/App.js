91982914b2eefae2a2aef7f1c05b6da026fac003
Vadim Brodsky <vadim.brodsky@gmail.com>
2017-10-08 23:35:58 -0400

    Add 2 more inline routes

--- js/react/fullstack-react/router/src/App.js
+++ js/react/fullstack-react/router/src/App.js
@@ -7,6 +7,7 @@ import {
   Link,
 } from 'react-router-dom';
 
+/* Mini Router
 const App = () => (
   <Router>
     <div className='ui text container'>
@@ -32,6 +33,49 @@ const App = () => (
 
       <hr />
 
+      <Route path='/atlantic' component={Atlantic} />
+      <Route path='/pacific' component={Pacific} />
+      <Route path='/black-sea' component={BlackSea} />
+    </div>
+  </Router>
+);
+*/
+
+// React Router
+const App = () => (
+  <Router>
+    <div className='ui text container'>
+      <h2 className='ui dividing header'>Which body of water?</h2>
+
+      <ul>
+        <li>
+          <Link to='/atlantic'>
+            <code>/atlantic</code>
+          </Link>
+        </li>
+        <li>
+          <Link to='/pacific'>
+            <code>/pacific</code>
+          </Link>
+        </li>
+        <li>
+          <Link to='/black-sea'>
+            <code>/black-sea</code>
+          </Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <Route path='/' render={() => (
+        <h3>Welcome! Select a body of saline water above.</h3>
+      )} />
+      <Route path='/atlantic/ocean' render={() => (
+        <div>
+          <h3>Atlantic Ocean - Again!</h3>
+          <p>Also known as "The Pond."</p>
+        </div>
+        )} />
       <Route path='/atlantic' component={Atlantic} />
       <Route path='/pacific' component={Pacific} />
       <Route path='/black-sea' component={BlackSea} />

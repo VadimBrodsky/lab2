d9aaaeae96d094a82210981274ea048b5f335607
Vadim Brodsky <vadim.brodsky@gmail.com>
2017-10-09 22:26:24 -0400

    Import mini redux

--- js/react/fullstack-react/redux/chat/src/App.js
+++ js/react/fullstack-react/redux/chat/src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { reducer, createStore } from './mini-redux';
+
+const initialState = { messages: [] };
+const store = createStore(reducer, initialState);
 
 class App extends Component {
   render() {

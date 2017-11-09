f244d5176c3903c0a51e8d7610733b35c1dc9ab1
Vadim Brodsky <vadim.brodsky@gmail.com>
2017-11-09 06:30:35 -0500

    Use mini redux and add Message components

--- js/react/fullstack-react/redux/chat/src/App.js
+++ js/react/fullstack-react/redux/chat/src/App.js
@@ -5,10 +5,79 @@ const initialState = { messages: [] };
 const store = createStore(reducer, initialState);
 
 class App extends Component {
+  componentDidMount() {
+    store.subscribe(() => this.forceUpdate());
+  }
+
+  render() {
+    const { messages } = store.getState();
+    return (
+      <div className='ui segment'>
+        <MessageView messages={messages} />
+        <MessageInput />
+      </div>
+    );
+  }
+}
+
+class MessageInput extends Component {
+  state = {
+    value: '',
+  };
+
+  onChange = e => {
+    this.setState({ value: e.target.value });
+  };
+
+  handleSubmit = () => {
+    store.dispatch({
+      type: 'ADD_MESSAGE',
+      message: this.state.value,
+    });
+    this.setState({ value: '' });
+  };
+
   render() {
     return (
-      <h1>Hello world</h1>
+      <div className='ui input'>
+        <input
+          onChange={this.onChange}
+          value={this.state.value}
+          type='text'
+        />
+        <button
+          onClick={this.handleSubmit}
+          className='ui primary button'
+          type='submit'
+        >
+          Submit
+        </button>
+      </div>
     );
+  }
+}
+
+class MessageView extends Component {
+  handleClick = index => {
+    store.dispatch({ type: 'DELETE_MESSAGE', index });
+  };
+
+  render() {
+    const messages = this.props.messages.map((message, index) => (
+      <div
+        className='comment'
+        key={index}
+        onClick={() => this.handleClick(index)}
+      >
+        {message}
+      </div>
+    ));
+
+    return (
+      <div className='ui components'>
+        {messages}
+      </div>
+    )
   }
 }
 

0596cb4aaf4f77e1c5ca8561309595a04457a1a8
Vadim Brodsky <vadim.brodsky@gmail.com>
2017-11-29 22:53:30 -0500

    Add button and list

--- js/react/redux/todo/src/index.js
+++ js/react/redux/todo/src/index.js
@@ -5,14 +5,36 @@ import './index.css';
 import reducers from './reducers';
 
 const store = createStore(reducers);
+let nextTodoId = 0;
 
-const TodoApp = () => {
-  return <h1>hello</h1>
+const TodoApp = ({ todos }) => {
+  return (
+    <div>
+      <button onClick={() => {
+        store.dispatch({
+          type: 'ADD_TODO',
+          text: 'Test',
+          id: nextTodoId++,
+        });
+      }}>
+        Add Todo
+      </button>
+      <ul>
+        {todos.map(todo =>
+          <li key={todo.id}>
+            {todo.text}
+          </li>
+        )}
+      </ul>
+    </div>
+  );
 };
 
 const render = () => {
   ReactDOM.render(
-    <TodoApp />,
+    <TodoApp
+      todos={store.getState().todos}
+    />,
     document.getElementById('root')
   );
 };

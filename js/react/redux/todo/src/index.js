d9ebbdc663ca77fc7945d17565a24135b063ed98
Vadim Brodsky <vadim.brodsky@gmail.com>
2017-12-07 22:07:34 -0500

    Move all of the store dispatch and subsribe calls to the wrapper components

--- js/react/redux/todo/src/index.js
+++ js/react/redux/todo/src/index.js
@@ -9,44 +9,16 @@ import AddTodo from './add-todo';
 import Footer from './footer';
 
 const store = createStore(reducers, applyMiddleware(logger));
-let nextTodoId = 0;
 
-const getVisibleTodos = (todos, filter) => {
-  switch(filter) {
-    case 'SHOW_ALL':
-      return todos;
-    case 'SHOW_COMPLETED':
-      return todos.filter(t => t.completed);
-    case 'SHOW_ACTIVE':
-      return todos.filter(t => !t.completed);
-  }
-}
-
-const TodoApp = ({ todos, visibilityFilter }) => (
+const TodoApp = () => (
   <div>
-    <AddTodo
-      onAddClick={text =>
-          store.dispatch({
-            type: 'ADD_TODO',
-            id: nextTodoId++,
-            text,
-          })
-      }
-    />
+    <AddTodo store={store} />
     <VisibleTodoList store={store} />
     <Footer store={store} />
   </div>
 );
 
-const render = () => {
-  ReactDOM.render(
-    <TodoApp
-      {...store.getState()}
-    />,
-    document.getElementById('root')
-  );
-};
-
-store.subscribe(render);
-render();
-
+ReactDOM.render(
+  <TodoApp />,
+  document.getElementById('root')
+);

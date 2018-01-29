ba9e1c3c8067c4ab6086b1c2a74a241980b906a0
Vadim Brodsky <vadim.brodsky@gmail.com>
2018-01-29 06:25:41 -0500

    Update initial store

--- js/react/redux/reduxtagram/src/store.js
+++ js/react/redux/reduxtagram/src/store.js
@@ -1,6 +1,26 @@
 import { createStore, compose } from 'redux';
+
 // import { syncHistoryWithStore } from 'react-router-redux';
 // import { browserHistory } from 'react-router';
+// Need to change these for RR V4 noop for now
+const syncHistoryWithStore = () => {};
+const browserHistory = {};
 
 // import root reducer
 import rootReducer from './reducers';
+
+// import sample data
+import comments from './data/comments';
+import posts from './data/posts';
+
+// create an object for default data
+const defaultState = {
+  posts,
+  comments,
+};
+
+const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
+export default store;

b11c6d4677e8da6b12feb6889619f800dc8384ba
Vadim Brodsky <vadim.brodsky@gmail.com>
2018-02-10 12:08:54 -0500

    Add jquery UI

--- js/react/redux/recipe-book/app.js
+++ js/react/redux/recipe-book/app.js
@@ -1,9 +1,11 @@
 import store from './store/store';
 import { addRecipe } from './actions/recipes';
 import { addIngredient } from './actions/ingredients';
+import loadUI from './ui/jquery';
 
 store.dispatch(addRecipe('Pancake'));
 store.dispatch(addIngredient('Pancake', 'Eggs', 3));
 
 window.store = store;
+loadUI();
 

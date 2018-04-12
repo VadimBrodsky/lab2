115acadacd7db3973ddaae012456b85bd69bfc6b
Vadim Brodsky <vadim.brodsky@gmail.com>
2018-04-11 21:35:10 -0400

    Add React Router

--- js/react/redux/todo/src/components/root.js
+++ js/react/redux/todo/src/components/root.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Provider from '../provider';
 import TodoApp from './app';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <TodoApp />
+    <Router>
+      <Route exact path="/" component={TodoApp} />
+    </Router>
   </Provider>
 );
 

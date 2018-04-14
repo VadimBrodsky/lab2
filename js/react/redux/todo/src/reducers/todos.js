f74b1998c3dcd8d3decc7fb239edd22d6c49b15a
Vadim Brodsky <vadim.brodsky@gmail.com>
2018-04-14 13:43:31 -0400

    Normalize the State shape, more DB like

--- js/react/redux/todo/src/reducers/todos.js
+++ js/react/redux/todo/src/reducers/todos.js
@@ -1,51 +1,46 @@
-const todo = (state, action) => {
+import { combineReducers } from 'redux';
+import todo from './todo';
+
+const allIds = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
-      return {
-        id: action.id,
-        text: action.text,
-        completed: false,
-      };
-    case 'TOGGLE_TODO':
-      if (state.id !== action.id) {
-        return state;
-      }
-      return {
-        ...state,
-        completed: !state.completed,
-      }
+      return [...state, action.id];
     default:
       return state;
   }
 };
 
-const todos = (state = [], action) => {
-  switch(action.type) {
+const byId = (state = {}, action) => {
+  switch (action.type) {
     case 'ADD_TODO':
-      return [
-        ...state,
-        todo(undefined, action),
-      ];
     case 'TOGGLE_TODO':
-      return state.map(t => todo(t, action));
+      return {
+        ...state,
+        [action.id]: todo(state[action.id], action),
+      };
     default:
       return state;
   }
-}
+};
 
+const todos = combineReducers({ byId, allIds });
+export default todos;
 
-// selector
+// selector, relies on the shape of the current state
 // exports that start with get, prepare the data to be displayed by the UI
 // select something from the current state
+
+const getAllTodos = (state) => state.allIds.map((id) => state.byId[id]);
+
 export const getVisibleTodos = (state, filter) => {
+  const allTodos = getAllTodos(state);
+
   switch (filter) {
     case 'all':
-      return state;
+      return allTodos;
     case 'completed':
-      return state.filter((t) => t.completed);
+      return allTodos.filter((t) => t.completed);
     case 'active':
-      return state.filter((t) => !t.completed);
+      return allTodos.filter((t) => !t.completed);
   }
 };
-
-export default todos;

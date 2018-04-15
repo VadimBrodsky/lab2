93b8b696d6ec6a78ce34414ff5c10840cd0d6791
Vadim Brodsky <vadim.brodsky@gmail.com>
2018-04-14 23:41:18 -0400

    Replace logger, promise and wrapDispathWithMiddleware with module code

--- js/react/redux/todo/src/configure-store.js
+++ js/react/redux/todo/src/configure-store.js
@@ -1,10 +1,46 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools, devToolsEnhancer } from 'redux-devtools-extension';
-// import logger from 'redux-logger';
+import promise from 'redux-promise';
+import { createLogger } from 'redux-logger';
 import throttle from 'lodash/throttle';
 // import { loadState, saveState } from './localstorage';
 import reducers from './reducers';
 
+const configureStore = () => {
+  const middlewares = [promise];
+
+  // replaced by server side storage
+  // const persistedState = loadState();
+
+  if (process.env.NODE_ENV !== 'production') {
+    middlewares.push(createLogger());
+  }
+
+  // wrapDispathWithMiddlewares(store, middlewares);
+
+  return createStore(
+    reducers,
+    // persistedState,
+    // devToolsEnhancer(),
+    // composeWithDevTools(applyMiddleware(logger)),
+    composeWithDevTools(applyMiddleware(...middlewares)),
+  );
+
+  /* replaced by server side storage
+  store.subscribe(
+    // throttle persistance for performance
+    throttle(
+      () =>
+        saveState({
+          todos: store.getState().todos,
+        }),
+      1000,
+    ),
+  );
+  */
+};
+
+/* logger middleware
 const logger = (store) => (next) => {
   if (!console.group) {
     return next;
@@ -21,54 +57,24 @@ const logger = (store) => (next) => {
     return returnValue;
   };
 };
+*/
 
+/* promise middleware
 const promise = (store) => (next) => (action) => {
   if (typeof action.then === 'function') {
     return action.then(next);
   }
   return next(action);
 };
+*/
 
+/* same as applyMiddleware
 const wrapDispathWithMiddlewares = (store, middlewares) => {
   middlewares
     .slice()
     .reverse()
     .forEach((middleware) => middleware(store)(store.dispatch));
 };
-
-const configureStore = () => {
-  // replaced by server side storage
-  // const persistedState = loadState();
-
-  const store = createStore(
-    reducers,
-    // persistedState,
-    devToolsEnhancer(),
-    // composeWithDevTools(applyMiddleware(logger)),
-  );
-
-  const middlewares = [store];
-
-  if (process.env.NODE_ENV !== 'production') {
-    middlewares.push(logger);
-  }
-
-  wrapDispathWithMiddlewares(store, middlewares);
-
-  /* replaced by server side storage
-  store.subscribe(
-    // throttle persistance for performance
-    throttle(
-      () =>
-        saveState({
-          todos: store.getState().todos,
-        }),
-      1000,
-    ),
-  );
-  */
-
-  return store;
-};
+*/
 
 export default configureStore;

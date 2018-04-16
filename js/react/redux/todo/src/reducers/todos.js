0252afa912eb948b74698a5ab8e2251999f7b9d4
Vadim Brodsky <vadim.brodsky@gmail.com>
2018-04-15 20:49:02 -0400

    WIP, update reducers for new server responses

--- js/react/redux/todo/src/reducers/todos.js
+++ js/react/redux/todo/src/reducers/todos.js
@@ -3,8 +3,17 @@ import todo from './todo';
 
 const allIds = (state = [], action) => {
   switch (action.type) {
-    case 'ADD_TODO':
-      return [...state, action.id];
+    case 'RECEIVE_TODOS':
+      return action.response.map((todo) => todo.id);
+    default:
+      return state;
+  }
+};
+
+const activeIds = (state = [], action) => {
+  switch (action.type) {
+    case 'RECEIVE_TODOS':
+      return action.response.map((todo) => todo.id);
     default:
       return state;
   }
@@ -23,15 +32,29 @@ const byId = (state = {}, action) => {
   }
 };
 
-const todos = combineReducers({ byId, allIds });
+const idsByFilter = combineReducers({
+  all: allIds,
+  active: activeIds,
+  completed: completedIds,
+});
+
+const todos = combineReducers({ byId, idsByFilter });
 export default todos;
+
+export const getVisibleTodos = (state, filter) => {
+  const ids = state.idsByFilter[filter];
+  return ids.map((id) => state.byId[id]);
+};
 
 // selector, relies on the shape of the current state
 // exports that start with get, prepare the data to be displayed by the UI
 // select something from the current state
 
+/* not practial to get all of the todos from the server
 const getAllTodos = (state) => state.allIds.map((id) => state.byId[id]);
+*/
 
+/* will not need to filter on the client
 export const getVisibleTodos = (state, filter) => {
   const allTodos = getAllTodos(state);
 
@@ -44,3 +67,4 @@ export const getVisibleTodos = (state, filter) => {
       return allTodos.filter((t) => !t.completed);
   }
 };
+*/
